1df612c4cc6fc94fa8473c9ee593908dd7a801a9
Andy <averyrandomname22@gmail.com>
2022-12-29 16:29:46 -0500

    AHHHHHH

---
+++ src/About/About.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+/* asijsdadsadgiutasjdiaj */
+const About = () => {
+  return (
+    <div className="title-section-container">
+        <div className="title-info-text">
+          <h1>
+           <span className="highlighted">About Us</span>
+          </h1>
+          <p className="title-info-description">
+            Free to use financial to use to keep you track with investments!
+          </p>
+          <img src="https://images.ladbible.com/resize?type=jpeg&quality=70&width=808&height=455&fit=contain&gravity=auto&dpr=1&url=https://eu-images.contentstack.com/v3/assets/blt949ea8e16e463049/bltba823561c6aaecda/63ac469335165d23c53964fe/doge-ill.jpeg" 
+          alt="Doge" srcSet=""/>
+          <p className="title-info-description">
+            AHHHHHHHHHsdads
+          </p>
+          <div className="search-container">
+
+          </div>
+          
+        </div>
+    
+    </div>
+  );
+};
+
+export default About;
---
+++ src/components/Intro.jsx
@@ -0,0 +1,98 @@
+import React, { useTransition } from 'react'
+import { Card } from './Card';
+
+const Intro = () => {
+
+  const features = [
+    {
+      // src: ,
+      title: "Chatbot",
+      description:
+        "Exists (trust me bro)",
+    },
+    {
+      // src: ,
+      title: "Data Visualization",
+      description:
+        "some graphs and forecasting stuff",
+    },
+    {
+      // src: ,
+      title: "News",
+      description: "Follow the most recent stuff for your stocks",
+    },
+    {
+      // src: ,
+      title: "Add/Delete Stocks",
+      description:
+        "Choose which stocks to keep track of",
+    },
+  ];
+
+  
+
+  return (
+    <div className="home">
+
+      <div className="header-container">
+          <div className="logo">InvestX</div>
+          <div className="menu">
+              <div className="menu-links">
+                  <a href="/">App</a>
+                  <a href="/demo">Demo</a>
+                  <a href="/about">About Us</a>
+                  <a href="/login">Sign In</a>
+              </div>
+          </div>
+          <div className="wallet-btn">
+              <button className="primary" href="/login">
+                Login In
+              </button>
+          </div>
+
+      </div>
+      <div className="title-section-container">
+          <div className="title-info-text">
+            <h1>
+              The <span className="highlighted">Financial</span> Tool to Succeed
+            </h1>
+            <p className="title-info-description">
+              Free to use financial to use to keep you track with investments!
+            </p>
+            <div className="search-container">
+          
+            </div>
+            
+          </div>
+      
+      </div>
+
+
+      <div className='features-list-container'>
+        {
+          features.map((feature, index) => {
+            return (
+              <Card key={index} title={feature.title} imgUrl={feature.src}>{<p>{feature.description}</p>}</Card>
+            )
+          })
+        }
+      </div>
+
+
+      <div className="footer-container">
+        <div className="footer-copyright">
+          <p>Copyright 2022 InvestX. All Rights Reserved</p>
+        </div>
+      </div>
+
+    </div>
+
+
+    
+  )
+}
+
+
+
+export default Intro;
+
