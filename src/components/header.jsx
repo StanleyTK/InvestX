25d9220775914a1252fec80e75dd2645ce39b2bf
Stanley Kim <kim4111@purdue.edu>
2022-12-29 12:32:42 -0500

    fox and andy sucks

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,20 +1,26 @@
-import React from 'react'
+import React, { useTransition } from 'react'
 
+import {Routes, Route, useNavigate} from 'react-router-dom';
+import signin from '../login/signin';
 const header = () => {
+  
+
   return (
     <div className="header-container">
         <div className="logo">InvestX</div>
         <div className="menu">
             <div className="menu-links">
-                <a href="/">App</a>
-                <a href="/">Demo</a>
-                <a href="/">About Us</a>
-                <a href="/">Sign Up</a>
+                <a href="/" onClick={signin}>App</a>
+                <a href="/demo">Demo</a>
+                <a href="/about">About Us</a>
+                <a href="/login">Sign In</a>
             </div>
         </div>
-        <div className="wallet-btn">
-            <button className="primary">Login In</button>
-        </div>
+        {/* <div className="wallet-btn">
+            <button className="primary" href="/login">
+              Login In
+            </button>
+        </div> */}
 
     </div>
 
@@ -22,4 +28,7 @@ const header = () => {
   )
 }
 
-export default header
+
+
+export default header;
+
